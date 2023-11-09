e828afde9bb7de059a4bcfe33bba4e1780951f67
marieern <150362309+marieern@users.noreply.github.com>
2023-11-09 15:49:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
- streamlit run uber_pickups.py
-
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
