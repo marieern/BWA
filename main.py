01242f6af161ec85855f1823e279b790e8063326
marieern <150362309+marieern@users.noreply.github.com>
2023-12-12 16:06:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,24 +30,12 @@ st.subheader('Bodenfeuchte')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-option = st.selectbox(
-    'Möchten Sie einen neuen Bewässerungsplan erstellen lassen?',
-    ('', 'Ja', 'Nein'))
-
-st.write('You selected:', option)
-
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     "Möchten Sie einen neuen Bewässerungsplan erstellen lassen?",
     ("Ja", "Nein")
 )
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+st.write('You selected:', add_selectbox)
 
 
             
