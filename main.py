f131bc900548350f550fe2c70ca2b598ca75dd46
marieern <150362309+marieern@users.noreply.github.com>
2023-12-12 15:59:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,5 +36,18 @@ option = st.selectbox(
 
 st.write('You selected:', option)
 
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
 
             
