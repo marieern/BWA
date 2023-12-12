7ad410c2a003543b9fd2509f858c18c21150cef1
marieern <150362309+marieern@users.noreply.github.com>
2023-12-12 15:38:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ data_load_state.text("Done! (using st.cache_data)")
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
-
-st.subheader('Bodenfeuchte')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
+st.subheader('Bodenfeuchte')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
 with st.chat_message("user"):
     st.write("Möchten Sie einen neuen Bewässerungsplan erstellen lassen?")
 
