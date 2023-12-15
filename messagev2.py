aab76b77cddd992ef440b7f1768afb15f9fd0129
marieern <150362309+marieern@users.noreply.github.com>
2023-12-15 09:50:38 +0100

    Update messagev2.py

--- messagev2.py
+++ messagev2.py
@@ -3,7 +3,7 @@ from streamlit_modal import Modal
 modal = Modal(key="Demo Key",title="test")
 for col in st.columns(8):
     with col:
-        open_modal = st.button(label='button')
+        open_modal = st.button(label='button', key='msg')
         if open_modal:
             with modal.container():
                 st.markdown('testtesttesttesttesttesttesttest')
