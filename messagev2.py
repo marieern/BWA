f52a3bc4614046cd94474718787bdeb770b4442f
marieern <150362309+marieern@users.noreply.github.com>
2023-12-15 09:48:30 +0100

    Create messagev2.py

---
+++ messagev2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_modal import Modal
+modal = Modal(key="Demo Key",title="test")
+for col in st.columns(8):
+    with col:
+        open_modal = st.button(label='button')
+        if open_modal:
+            with modal.container():
+                st.markdown('testtesttesttesttesttesttesttest')
