f10d13acee5cd969b3e3e4b511296780c1ab0cf3
marieern <150362309+marieern@users.noreply.github.com>
2023-12-15 09:56:15 +0100

    Update messagev2.py

--- messagev2.py
+++ messagev2.py
@@ -8,3 +8,4 @@ for col in st.columns(8):
             with modal.container():
                 st.markdown('Es gibt Abweichungen der aktuellen Bodenfeuchte von der Oprimalen. Es wird geraten einen neuen Bewässerungsplan anhand der aktuellen Daten zu erstellen. Möchten Sie dies tun?')
                 st.button(label='ja', key='ja')
+                st.button(label='nein', key='nein')
