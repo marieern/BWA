ade584ee29d253f07b0355e981aed3e0497c95de
marieern <150362309+marieern@users.noreply.github.com>
2023-12-21 14:05:20 +0100

    Create msg5.py

---
+++ msg5.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_modal import Modal
+
+modal = Modal(key="Demo Key",title="test")
+for col in st.columns(8):
+    with col:
+        with modal.container():
+            st.markdown('Es gibt Abweichungen der aktuellen Bodenfeuchte von der Oprimalen. Es wird geraten einen neuen Bewässerungsplan anhand der aktuellen Daten zu erstellen. Möchten Sie dies tun?')
+            st.button(label='ja', key='ja')
+            st.button(label='nein', key='nein')
